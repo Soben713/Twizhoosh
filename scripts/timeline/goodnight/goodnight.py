cc6a986d112f0cfab32229aceb3542f67c05291e
Soheil <soheil.behnezhad@gmail.com>
2015-01-03 02:01:15 +0330

    Add dot at the end of goodnight regex

--- scripts/timeline/goodnight/goodnight.py
+++ scripts/timeline/goodnight/goodnight.py
@@ -7,7 +7,7 @@ from scripts.timeline.goodnight.goodnight_responses import goodnight_responses
 
 class GoodNight(smart_reply_by_keyword.SmartReplyByKeyword):
     replies = [{
-                   'keywords': [r'^(#)?شب(تون)?(\s|‌)*(بخیر|به خیر|به‌خیر|خوش)$'],
+                   'keywords': [r'^(#)?شب(تون)?(\s|‌)*(بخیر|به خیر|به‌خیر|خوش)(.)?$'],
                    'reply_messages': goodnight_responses
                }]
     answer_once = True
