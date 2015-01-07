d70c83020ecdfe4a91e67bbba54db8e816441d12
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2015-01-08 03:09:31 +0330

    Use another method to retrieve uscreen names in dedicate round tweets

--- scripts/twitter_related/dedicate_round_tweets/dedicate_round_tweets.py
+++ scripts/twitter_related/dedicate_round_tweets/dedicate_round_tweets.py
@@ -18,37 +18,35 @@ def cons_numbers(x):
             return False
     return True
 
+regex_patterns = [
+    r'^[0-9]+00$',  # Numbers that end with 00
+    r'^([0-9])\1+$',  # Numbers that are all equal
+]
+
+function_patterns = [
+    cons_numbers,  # Consecutive numbers, like 2345 or 8765
+]
+
+def is_round(num):
+    if len(str(num)) < 3:
+        return
+    for regex in regex_patterns:
+        if re.match(regex, str(num)):
+            return True
+    for f in function_patterns:
+        if f(str(num)):
+            return True
+    return False
+
 
 class DedicateRoundTweets(base.BaseOnSelfStatusUpdate):
-    regex_patterns = [
-        r'^[0-9]+00$',  # Numbers that end with 00
-        r'^([0-9])\1+$',  # Numbers that are all equal
-    ]
-
-    function_patterns = [
-        cons_numbers,  # Consecutive numbers, like 2345 or 8765
-    ]
-
-    def is_round(self, num):
-        if len(str(num)) < 3:
-            return
-        for regex in self.regex_patterns:
-            if re.match(regex, str(num)):
-                return True
-        for f in self.function_patterns:
-            if f(str(num)):
-                return True
-        return False
-
     def dedicate_to(self):
         if not settings.DEBUG:
             friends = self.twitter.twitter.get_friends_ids(screen_name=settings.TWIZHOOSH_USERNAME)
             log("found friends: " + str(friends))
-            id = random.choice(friends['ids'])
-            try:
-                dedicated_to = self.twitter.twitter.show_user(user_id=id)['screen_name']
-            except TwythonError:
-                log("Last call headers:\n + " + str(self.twitter.twitter._last_call))
+            id = str(random.choice(friends['ids']))
+            # Should be show_user since we're catching info for one user, but twitter returns nothing, idk why
+            dedicated_to = self.twitter.twitter.show_user(user_id=id)['screen_name']
             log("dedicate to: " + dedicated_to)
         else:
             dedicated_to = 'tester'
@@ -58,7 +56,12 @@ class DedicateRoundTweets(base.BaseOnSelfStatusUpdate):
         tweets = data['user']['statuses_count']
         log("Number of tweets: " + str(tweets))
 
-        if self.is_round(tweets + 1):
+        if is_round(tweets + 1):
             log("Next tweet is round")
-            status = 'توییت {0} تقدیم به {1}.'.format(str(tweets + 1), '@' + self.dedicate_to())
+            try:
+                status = 'توییت {0} تقدیم به {1}.'.format(str(tweets + 1), '@' + self.dedicate_to())
+            except TwythonError as e:
+                log("Twython error: " + str(e))
+                log("Last call headers:\n + " + str(self.twitter.twitter._last_call))
+                status = 'توییت {0} رو هم تقدیم نمی‌کنیم...'.format(str(tweets + 1))
             self.twitter.tweet(status=status)
