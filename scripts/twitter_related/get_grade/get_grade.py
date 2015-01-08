d5df175a5d5264ff371ea87caa40e14a49ba105a
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2015-01-08 23:43:24 +0330

    Add probabilities to grades

--- scripts/twitter_related/get_grade/get_grade.py
+++ scripts/twitter_related/get_grade/get_grade.py
@@ -9,8 +9,23 @@ class GetGrade(on_demand.BaseOnTimelineDemandScript):
     command_pattern = r'.*چند می( |‌)?ش(و)?م.*'
 
     def received_command(self, command, data):
-        match = re.search(self.command_pattern, command)
+        if re.search(self.command_pattern, command):
+            # Probability of the grade being between second and third items
+            probs = [
+                (0.1, 20, 20),
+                (0.4, 15, 19.9),
+                (0.25, 12, 14.9),
+                (0.10, 10, 11.9),
+                (0.13, 7, 9.9),
+                (0.02, 0, 6.9)
+            ]
 
-        if match:
-            log("{0} asked for grade".format(data['user']['screen_name']))
-            self.twitter.reply_to(data, "{0:.1f}".format(min(random.random() * 30, 20)))
+            rand = random.random()
+            _x = 0.0
+            for i in range(len(probs)):
+                _x += probs[i][0]
+                if rand <= _x:
+                    grade = random.random() * (probs[i][2] - probs[i][1]) + probs[i][1]
+                    break
+
+            self.twitter.reply_to(data, "{0:.1f}".format(grade))
