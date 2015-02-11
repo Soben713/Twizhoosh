7088cb811c670cd5c0ea1c1fb0ee844b1011507a
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2015-02-11 22:39:55 +0330

    Update update_docs.py

--- update_docs.py
+++ update_docs.py
@@ -11,25 +11,19 @@ import settings
 twitter_related_scripts = settings.INSTALLED_TWITTER_RELATED_SCRIPTS
 
 template = \
-    '''---
-    layout: post
-    title: %(title)s
-    source: %(source)s
-    ---
-    %(content)s
-    '''
+    '---' \
+    'layout: post' \
+    'title: %(title)s' \
+    'source: %(source)s' \
+    '---' \
+    '%(content)s' \
 
 source = 'https://github.com/Soben713/Twizhoosh/tree/master/scripts/twitter_related/%s'
 
 
 def delete_folder_contents(folder_path):
-    for the_file in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, the_file)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(e)
+    os.system('rm -rf _posts')
+    os.system('mkdir _posts')
 
 
 def collect_docs():
