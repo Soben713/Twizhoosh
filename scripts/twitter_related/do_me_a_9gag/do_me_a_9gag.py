e859d0010d9d7926c5379409fd5ffb3e54be5973
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2015-02-17 23:35:09 +0330

    Change do me a 9gag to post the picture

--- scripts/twitter_related/do_me_a_9gag/do_me_a_9gag.py
+++ scripts/twitter_related/do_me_a_9gag/do_me_a_9gag.py
@@ -1,14 +1,48 @@
+from io import BytesIO
 import re
 from urllib import request
 
+from bs4 import BeautifulSoup
+import requests
 from core.scripts.twitter_related import on_demand
+from core.utils.logging import log
+import settings
 
 
-class DoMeA9gag(on_demand.BaseOnDirectMessageOrTimelineDemandScript):
+class DoMeA9gag(on_demand.BaseOnTimelineDemandScript):
     command_pattern = r'.*9gag.*'
 
     def received_command(self, command, data, reply_message, sender, *args, **kwargs):
         if re.search(self.command_pattern, command, re.IGNORECASE):
-            r = request.Request('http://9gag.com/random')
-            res = request.urlopen(r)
-            reply_message(res.geturl())
+            while True:
+                r = request.Request('http://9gag.com/random')
+                opened = request.urlopen(r)
+                parsed = BeautifulSoup(opened)
+
+                log("Found 9gag link: %s" % (opened.geturl()))
+
+                if parsed.select('.gif-post'):
+                    # Ignore gif posts
+                    log("Ignoring... was a gif post")
+                    continue
+
+                try:
+                    log("Trying to send %s" % opened.geturl())
+                    img_url = parsed.select('.badge-item-img')[0].get('src')
+                    title = parsed.select('.badge-item-title')[0].getText()
+
+                    img = requests.get(url=img_url).content
+
+                    status = '@%s %s - %s' % (sender['screen_name'], title, opened.geturl())
+                    if len(status) > settings.TWEET_LENGTH:
+                        status = '@%s %s' % (sender['screen_name'], title)
+
+                    self.twitter.update_status_with_media(
+                        status=status,
+                        media=BytesIO(img),
+                        in_reply_to_status_id=data['id_str']
+                    )
+
+                    break
+                except Exception:
+                    pass
